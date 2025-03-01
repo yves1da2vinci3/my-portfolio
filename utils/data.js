96f694037b5a58160b02aa4bcebed23e151eb925
yves1da2vinci3 <diomadelacorano@gmail.com>
2025-03-01 19:43:55 +0000

    Ajouter le statut du projet et l'icône Android pour les projets mobiles

--- utils/data.js
+++ utils/data.js
@@ -1,13 +1,8 @@
 import alia from '../public/PreviewImages/alia.png';
-import Blog from '../public/PreviewImages/blogPreview.png';
-import Bolstarr from '../public/PreviewImages/bolstar.png';
-import BolstarUI from '../public/PreviewImages/BolstarUI.png';
 import ChoseYourCamp from '../public/PreviewImages/choseYourCamp.png';
 import convserseWithMe from '../public/PreviewImages/converseWithMe.jpeg';
-import designMyPDF from '../public/PreviewImages/designMypdf.png';
-import djamo from '../public/PreviewImages/djamo.png';
+import designMyPDF from '../public/PreviewImages/designMyPdf.jpeg';
 import green from '../public/PreviewImages/greenDeli.png';
-import houmklo from '../public/PreviewImages/houmklo.png';
 import meet from '../public/PreviewImages/meet.png';
 import mongoGeniusPreview from '../public/PreviewImages/mongoGenius.png';
 import Quizz from '../public/PreviewImages/onLineApp.png';
@@ -15,7 +10,6 @@ import ResidenceIT from '../public/PreviewImages/residenceIt.png';
 import service from '../public/PreviewImages/serviceProvider.png';
 import TantieFatou from '../public/PreviewImages/tantieFatou.png';
 import Tiktok from '../public/PreviewImages/tiktok.png';
-import todo from '../public/PreviewImages/todo.png';
 import UBank from '../public/PreviewImages/ubank.png';
 import WhatsApp from '../public/PreviewImages/whatsapp.png';
 import WhatsAppWeb from '../public/PreviewImages/whatsappWeb.png';
@@ -40,6 +34,7 @@ const projects = [
     ],
     githubLink: 'https://github.com/yves1da2vinci3/MongoGenius-frontend',
     openLink: 'https://mongo-genius-front.vercel.app/',
+    status: 0,
   },
   {
     title: 'DesignMyPDF',
@@ -59,7 +54,8 @@ const projects = [
       'uuid-ossp',
     ],
     githubLink: 'https://github.com/',
-    openLink: 'https://designmypdf.vercel.app/',
+    openLink: 'https://design-my-pdf.vercel.app/',
+    status: 0,
   },
   {
     title: 'Conference App ',
@@ -80,6 +76,7 @@ const projects = [
     ],
     githubLink: 'https://github.com/',
     openLink: 'https://mygooglemeet.netlify.app/',
+    status: 1,
   },
   {
     title: 'Online Multiplayer QuizzApp',
@@ -100,6 +97,7 @@ const projects = [
     ],
     githubLink: 'https://github.com/',
     openLink: 'https://onlinequizzapp.netlify.app/',
+    status: 1,
   },
   {
     title: 'ChoseYourCamp',
@@ -112,6 +110,7 @@ const projects = [
     technologies: ['Nextjs', 'Tailwindcss', 'Nestjs', 'Mongodb', 'JWT'],
     githubLink: 'https://github.com/',
     openLink: 'https://chose-your-camp-frontend.vercel.app/articles',
+    status: 0,
   },
 
   {
@@ -125,18 +124,8 @@ const projects = [
     technologies: ['Reactjs', 'CSS', , 'mantine UI'],
     githubLink: 'https://github.com/',
     openLink: 'https://alia-legal.com/',
+    status: 1,
   },
-  // {
-  //   title: "MusicStreaming",
-  //   type: "web",
-  //   projectType: 1,
-  //   preview: musique,
-  //   subtitle: "Plateforme de streaming de musique",
-  //   description: "Il s'agit d'une application de streaming de musique",
-  //   technologies: ["Reactjs", "TailwindCss", "React Router", "Mantine"],
-  //   githubLink: "https://github.com/",
-  //   openLink: "https://musicappstreaming.netlify.app/",
-  // },
   {
     title: 'TantieFatou',
     type: 'web',
@@ -148,6 +137,7 @@ const projects = [
     technologies: ['HTML', 'CSS', 'JS', 'Scroll Reveal', 'Box Icons'],
     githubLink: 'https://github.com/',
     openLink: 'https://tantiefatou.netlify.app/',
+    status: 1,
   },
   {
     title: 'WebWhatsApp',
@@ -166,6 +156,7 @@ const projects = [
     ],
     githubLink: 'https://github.com/',
     openLink: 'https://webwhatsappclone.netlify.app/',
+    status: 1,
   },
   {
     title: 'ResidenceIT',
@@ -186,42 +177,31 @@ const projects = [
     ],
     githubLink: 'https://github.com/',
     openLink: 'https://residenceit.netlify.app/',
+    status: 1,
   },
   {
-    title: 'Bolstarr',
-    type: 'web',
-    projectType: 1,
-    preview: Bolstarr,
-    subtitle:
-      'Un site qui met en relation les agriculteurs et les fourniesseurs outils agricole',
-    description: 'La gestion des produits ,commande',
-    technologies: ['Reactjs', 'Tailwindcss', 'RecoilsJs'],
-    githubLink: 'https://github.com/',
-    openLink: 'https://bolstar.netlify.app/',
-  },
-  {
-    title: 'Todo',
-    type: 'web',
-    projectType: 1,
-    preview: todo,
-    subtitle: 'Une application de gestion de tâches',
+    title: 'UBank',
+    type: 'mobile',
+    projectType: 2,
+    preview: UBank,
+    subtitle: 'Une application de suivi des dépenses avec assistant IA',
     description:
-      "Une application qui gere les taches muni d'un systeme d'authentification le tout effectue sans une base de données",
-    technologies: ['HTML', 'CSS', 'JS', 'LocalStorage'],
+      'Cette application permet de suivre vos dépenses et revenus avec un assistant financier IA intégré qui vous aide à analyser vos habitudes financières et vous donne des conseils personnalisés pour une meilleure gestion de votre argent',
+    technologies: [
+      'React Native',
+      'Context Api',
+      'React-native-charts',
+      'Nativewind',
+      'FastAPI',
+      'PostgresQL',
+      'MINIO',
+      'OpenAI',
+      'Langchain',
+    ],
     githubLink: 'https://github.com/',
-    openLink: 'example',
-  },
-  {
-    title: 'Houmklo',
-    type: 'web',
-    projectType: 1,
-    preview: houmklo,
-    subtitle: 'Un site pour les designers',
-    description:
-      "Il s'agit d'une plateforme qui permet aux designers de se connecter ,de faire la veille technologique ,de poster leurs creations et ecrire des articles.",
-    technologies: ['Reactjs', 'Tailwindcss', 'ExpressJs', 'Nodejs', 'JWT'],
-    githubLink: 'https://github.com/',
-    openLink: 'https://houmklodesign.netlify.app/',
+    openLink:
+      'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FWhatsApp%20Video%202024-11-08%20at%2007.56.13.mp4?alt=media&token=c6c0976f-5310-4099-8070-53dc9411347d',
+    status: 0,
   },
   {
     title: 'Fixit',
@@ -244,6 +224,8 @@ const projects = [
     githubLink: 'https://github.com/',
     openLink:
       'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FSimulator%20Screen%20Recording%20-%20iPhone%2013%20-%202023-07-19%20at%2002.32.19.mp4?alt=media&token=74be90e9-82f0-4a97-b999-f1f5948c6fad',
+    apkLink: 'https://example.com/fixit.apk',
+    status: 1,
   },
   {
     title: 'Tiktok',
@@ -263,6 +245,7 @@ const projects = [
     githubLink: 'https://github.com/',
     openLink:
       'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2Ffd.mp4?alt=media&token=08d8a70f-012d-462e-b960-dfca7ca33fa7',
+    status: 0,
   },
   {
     title: 'MessageMe',
@@ -287,6 +270,7 @@ const projects = [
     githubLink: 'https://github.com/',
     openLink:
       'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FSimulator%20Screen%20Recording%20-%20iPhone%2013%20-%202023-07-17%20at%2000.33.04.mp4?alt=media&token=df5eae76-0fd2-4e57-943d-66851d35733a',
+    status: 0,
   },
 
   {
@@ -306,29 +290,7 @@ const projects = [
     githubLink: 'https://github.com/',
     openLink:
       'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FconverseWithMe.mp4?alt=media&token=56ec3204-4f33-4efb-9ef2-fb74dbda8c82',
-  },
-  {
-    title: 'UBank',
-    type: 'mobile',
-    projectType: 2,
-    preview: UBank,
-    subtitle: 'Une application de suivi des dépenses avec assistant IA',
-    description:
-      'Cette application permet de suivre vos dépenses et revenus avec un assistant financier IA intégré qui vous aide à analyser vos habitudes financières et vous donne des conseils personnalisés pour une meilleure gestion de votre argent',
-    technologies: [
-      'React Native',
-      'Context Api',
-      'React-native-charts',
-      'Nativewind',
-      'FastAPI',
-      'PostgresQL',
-      'MINIO',
-      'OpenAI',
-      'Langchain',
-    ],
-    githubLink: 'https://github.com/',
-    openLink:
-      'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FWhatsApp%20Video%202024-11-08%20at%2007.56.13.mp4?alt=media&token=c6c0976f-5310-4099-8070-53dc9411347d',
+    status: 0,
   },
   {
     title: 'GreenDeli',
@@ -345,45 +307,7 @@ const projects = [
     githubLink: 'https://github.com/',
     openLink:
       'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FSimulator%20Screen%20Recording%20-%20iPhone%2013%20-%202023-01-16%20at%2022.58.02.mp4?alt=media&token=3efd2437-1295-4ff4-b652-da1480f41d41',
-  },
-  {
-    title: 'Blog App',
-    type: 'mobile',
-    projectType: 2,
-    preview: Blog,
-    subtitle: 'Un Linkedin pour designers ',
-    description:
-      "L'application permet aux designers d'avoir une plateforme mobile ou ils peuvent discuter et partager leur creation sans souci",
-    technologies: ['React native', 'Twrnc', 'Expo Icons'],
-    githubLink: 'https://github.com/',
-    openLink:
-      'https://firebasestorage.googleapis.com/v0/b/myportfolio-527e2.appspot.com/o/VideoServices%2FSimulator%20Screen%20Recording%20-%20iPhone%2013%20-%202023-07-20%20at%2000.11.15.mp4?alt=media&token=5991444d-689f-4829-a77f-28c252f76570',
-  },
-  {
-    title: 'Djamo',
-    type: 'Ui',
-    projectType: 3,
-    preview: djamo,
-    subtitle: 'Redesign du Site de djamo.ci',
-    description: 'Redesign du Site de djamo en choissisant des termes darks',
-    technologies: ['ADOBE XD', 'ADOBE PHOTOSHOP', 'Adobe Illustrator'],
-    githubLink: 'https://github.com/',
-    openLink:
-      'https://xd.adobe.com/view/999b9794-bd5b-4c03-ac19-2c06a6de8284-ccc8/?fullscreen',
-  },
-  {
-    title: 'BolstarUi',
-    type: 'Ui',
-    projectType: 3,
-    preview: BolstarUI,
-    subtitle:
-      "Design de l'application web de mise en relation entre agriculteurs et fournisseurs de produits agricoles ",
-    description:
-      "Design de l'application web de mise en relation entre agriculteurs et fournisseurs de produits agricoles",
-    technologies: ['ADOBE XD', 'ADOBE PHOTOSHOP', 'Adobe Illustrator'],
-    githubLink: 'https://github.com/',
-    openLink:
-      'https://xd.adobe.com/view/1cc84212-fbe5-4511-9e92-d85b5f884699-499b/screen/93b526d1-4a45-44bc-b0c1-1d4ec60b8b5b?fullscreen',
+    status: 1,
   },
 ];
 
